7ed41260b6e660c099fdb542b9d447ed096cc669
Giao U <lesliegiao.u@gmail.com>
2024-07-04 20:24:41 -0500

    feature-1/render two products and separate ProductListItem from MenuScreen

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,36 +1,12 @@
 import products from "@/assets/data/products";
-import { Image, StyleSheet, Text, View } from "react-native";
-import Colors from "../../constants/Colors";
+import ProductListItem from "@/src/components/ProductListItem";
+import { View } from "react-native";
 
-const product = products[3];
-
-export default function TabOneScreen() {
+export default function MenuScreen() {
   return (
-    <View style={styles.container}>
-      <Image source={{ uri: product.image }} style={styles.image} />
-      <Text style={styles.title}>{product.name}</Text>
-      <Text style={styles.price}>${product.price}</Text>
+    <View>
+      <ProductListItem product={products[4]} />
+      <ProductListItem product={products[1]} />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    backgroundColor: "white",
-    padding: 10,
-    borderRadius: 20,
-  },
-  image: {
-    width: "100%",
-    aspectRatio: 1, //height will be dependent on width or vice versa
-  },
-  title: {
-    fontSize: 18,
-    fontWeight: "600",
-    marginVertical: 10,
-  },
-  price: {
-    color: Colors.light.tint,
-    fontWeight: "bold",
-  },
-});
