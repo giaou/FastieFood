371b685102900052a8ebc2a60b82a9c48c90c7b7
Giao U <lesliegiao.u@gmail.com>
2024-07-08 18:21:56 -0500

    Add a header on the right side on the top of menu page

---
+++ src/app/cart.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+const CartScreen = () => {
+  return (
+    <View>
+      <Text>Hello This is your cart</Text>
+    </View>
+  );
+};
+
+export default CartScreen;
