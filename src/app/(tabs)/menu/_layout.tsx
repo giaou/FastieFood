955381195146c2d9f0a3f435f8fb3b5e7617c4c3
Giao U <lesliegiao.u@gmail.com>
2024-07-08 18:23:09 -0500

    when click at theheader icon, it will lead to cart page

--- src/app/(tabs)/menu/_layout.tsx
+++ src/app/(tabs)/menu/_layout.tsx
@@ -5,22 +5,24 @@ import { Pressable } from "react-native";
 
 export default function MenuStack() {
   return (
-    <Stack screenOptions={{
-      headerRight: () => (
-        <Link href="/modal" asChild>
-          <Pressable>
-            {({ pressed }) => (
-              <FontAwesome
-                name="info-circle"
-                size={25}
-                color={Colors.light.tint}
-                style={{ marginRight: 15, opacity: pressed ? 0.5 : 1 }}
-              />
-            )}
-          </Pressable>
-        </Link>
-      ),
-    }}>
+    <Stack
+      screenOptions={{
+        headerRight: () => (
+          <Link href="/cart" asChild>
+            <Pressable>
+              {({ pressed }) => (
+                <FontAwesome
+                  name="shopping-cart"
+                  size={25}
+                  color={Colors.light.tint}
+                  style={{ marginRight: 15, opacity: pressed ? 0.5 : 1 }}
+                />
+              )}
+            </Pressable>
+          </Link>
+        ),
+      }}
+    >
       <Stack.Screen name="index" options={{ title: "Menu" }} />
     </Stack>
   );
